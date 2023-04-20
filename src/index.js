5c52fa5c98f0c4756955d32d38b5e8ad448a48c2
PaosLuna <lunapp20@gmail.com>
2023-04-20 14:03:39 -0600

    Tarjeta 4, ya dice si es valida

--- src/index.js
+++ src/index.js
@@ -2,6 +2,74 @@
 import validator from './validator.js';
 console.log(validator);
 
+//TRAER NÚMERO DE LA TARJETA
+let numeroCard = document.querySelector(".numero_falso");
+let numeroInput = document.querySelector("#tarjeta-cliente");
+let errorCard = document.querySelector(".error");
+let comprar = document.getElementById("comprar");
+
+//GUARDAR EL NÚMERO DE TARJETA Y CONVERTIR EN ARRAY
+    comprar.addEventListener("click", event=>{
+    let tarjetaCliente = document.getElementById("tarjeta-cliente").value;
+    validator.isValid(tarjetaCliente); //VIENE DE VALIDATOR.JS
+      
+  //LLAMAR DE VALIDATOR PARA QUE DIGA SI ES VALIDA O NO   
+  //  function algoritmoLuhn(){
+  //      validator.isValid(cadena); 
+  //  if (sumar == true){
+  //          console.log("Tu tarjeta es valida");
+  //      } else if (sumar == false) {
+  //         console.log("Tu tarjeta es invalida");
+  //        }
+  //      }
+
+});
+
+
+
+
+
+//comprar.addEventListener("click", algoritmoLuhn);
+
+
+
+
+
+//LLENAR NÚMERO EN TARJETA DIGITAL
+numeroInput.addEventListener('input', event=>{
+    let inputValue = event.target.value;
+    let regExp = /[A-z]/g; //MARCA ERROR SI SALE LETRA
+    
+    if(regExp.test(numeroInput.value) == true){
+            errorCard.innerText = "Sólo números";
+        }else{//QUITA Y PONE ESPACIOS 
+           numeroInput.value = inputValue.replace(/\s/g, '').trim()
+          // replace(/([0-9]{4})/g, '$1 ')
+           errorCard.innerText = '';
+    }  
+    
+    if(numeroInput.value == ''){ //DUPLICAR NÚMERO EN TARJETA DIGITAL
+        numeroCard.innerText = "0000 0000 0000 0000"
+     }else{
+         numeroCard.innerText = numeroInput.value;
+     }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//SOLO SIRVE PARA LLENAR LA TARJETA DIGITAL 
+
+
 //TRAER NOMBRE DE LA TARJETA
 let nombreCard = document.querySelector(".datos-tarjeta-nombre");
 let nombreInput = document.querySelector("#nombre-cliente");
@@ -14,41 +82,6 @@ nombreInput.addEventListener("input", ()=>{
     nombreCard.innerText = nombreInput.value;
     }
 });
-
-//TRAER NÚMERO DE LA TARJETA
-let numeroCard = document.querySelector(".numero_falso");
-let numeroInput = document.querySelector("#tarjeta-cliente");
-let errorCard = document.querySelector(".error");
-
-
-
-//GUARDAR EL NÚMERO DE TARJETA Y CONVERTIR EN ARRAY
-    numeroInput.addEventListener("input", event=>{
-    let tarjetaCliente = document.getElementById("tarjeta-cliente").value;
-    validator.isValid(tarjetaCliente); //VIENE DE VALIDATOR.JS
-});
-
-
-
-//NÚMERO DE TARJETA 
-numeroInput.addEventListener('input', event=>{
-    let inputValue = event.target.value;
-    let regExp = /[A-z]/g; //MARCA ERROR SI SALE LETRA
-    
-    if(regExp.test(numeroInput.value) == true){
-            errorCard.innerText = "Sólo números";
-        }else{//QUITA Y PONE ESPACIOS 
-            numeroInput.value = inputValue.replace(/\s/g, '').replace(/([0-9]{4})/g, '$1 ').trim()
-            errorCard.innerText = '';
-    }  
-    
-    if(numeroInput.value == ''){ //DUPLICAR NÚMERO EN TARJETA DIGITAL
-        numeroCard.innerText = "0000 0000 0000 0000"
-     }else{
-         numeroCard.innerText = numeroInput.value;
-     }
-});
-
 
 //TRAER FECHA DE LA TARJETA
 let mesCard = document.querySelector(".datos-tarjeta-mes");
@@ -74,17 +107,3 @@ añoInput.addEventListener('input', ()=>{
     }
 
 });
-
-
-
-
-
-
-
-//function infoCliente() {
-//    let tarjetaCliente = document.getElementById("tarjeta-cliente");
-//    localStorage.setItem("cardCliente", tarjetaCliente.value);
-//    console.log(localStorage.getItem("cardCliente"));
-//}
-
-
--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,36 @@
 const validator = {
   isValid:function(cadena){
     let cardCliente = cadena.split(""); //convertir en array
-    console.log(cardCliente);
+    let reversaCard = cardCliente.reverse(); //invertir 
+    console.log(reversaCard);
+
+    for (var i=0; i<reversaCard.length; i++){
+      if (i%2 != 0){ // busca los indices de posicion par
+        reversaCard[i] = reversaCard[i] * 2; // multiplica los pares x2
+      }
+      if (reversaCard[i] > 9){ //ve si el índice es mayor a 9
+      reversaCard[i] = parseInt(String(reversaCard[i]).charAt(0)) + parseInt(String(reversaCard[i]).charAt(1)) //suma los 2 digitos
+        console.log(reversaCard);
+      }
+    } 
+
+      var sumar = 0; //declaramos la suma para empezar en 0
+      
+      for (var i=0; i<reversaCard.length; i++) {
+        sumar += parseInt(reversaCard[i]); //suma todos los digitos
+        console.log(sumar);
+      }
+    
+     var sumar = sumar * 9 % 10; //se mutiplica y divide 
+      console.log(sumar);
+
+      if (sumar == 0){ //condicional para ver si es valida
+        alert("Tu tarjeta es valida")
+        return true
+      }else{
+        alert("Tu tarjeta es invalida")
+        return false
+      }      
   },
 
 
