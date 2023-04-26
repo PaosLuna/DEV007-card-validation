675b8918dc157795bcf83c3ac4c7461c78ad0a60
PaosLuna <lunapp20@gmail.com>
2023-04-26 15:18:54 -0600

    Updates

---
+++ index.js
@@ -0,0 +1,179 @@
+//4083952015263
+import validator from './validator.js';
+console.log(validator);
+
+//TRAER NÚMERO DE LA TARJETA
+const numeroCard = document.querySelector(".numero_falso");
+const numeroInput = document.querySelector("#tarjeta-cliente");
+//const errorCard = document.querySelector(".error");
+const comprar = document.getElementById("comprar");
+
+//LOGOS DEL BANCO
+const logoVisa = document.getElementById("logo-visa");
+const logoMaster = document.getElementById("logo-master");
+const logoAmerican = document.getElementById("logo-american");
+
+logoVisa.style.display = 'none';
+logoMaster.style.display = 'none';
+logoAmerican.style.display = 'none';
+
+//QUÉ SUCEDE AL INGRESAR EL NÚMERO EN EL INPUT
+numeroInput.addEventListener('input', event => {
+  const inputValue = event.target.value;
+  numeroInput.value = inputValue.replace(/\s/g, '').replace(/\D/g, '').trim() //NO DEJA INGRESAR LETRAS NI ESPACIOS
+
+  //LLENAR Y ENMASCARAR TARJETA DIGITAL
+  if (numeroInput.value === '') { //DUPLICAR NÚMERO EN TARJETA DIGITAL
+    numeroCard.innerText = "0000 0000 0000 0000"
+  } else {
+    numeroCard.innerText = numeroInput.value.slice(0, -4).replace(/./g, "#") + numeroInput.value.slice(-4); //MUESTRA LA MÁSCARA EN LA TD
+  }
+  console.log(inputValue);
+  
+  //CONOCER EL BANCO DE LA TARJETA  
+  const numero = inputValue.slice(0, 2);
+  console.log(numero);
+  if (numero === "40"){
+    //console.log("VISA");
+    visa();
+  } if (numero === "34" || numero === "37") {
+    //console.log("AMERICAN EXPRESS"); 
+    master();
+  } if (numero === "51" || numero === "55") {
+    //console.log("MASTER CARD");
+    american();
+  } 
+});
+
+//MOSTRAR LOGO BANCO
+function visa(){
+  logoVisa.style.display = 'block';
+}
+
+function master(){
+  logoMaster.style.display = 'block';
+}
+
+function american(){
+  logoAmerican.style.display = 'block';
+}
+
+
+
+
+
+//QUÉ SUCEDE AL DAR CLICK EN COMPRAR
+comprar.addEventListener("click", () => {
+  const tarjetaCliente = document.getElementById("tarjeta-cliente").value;
+  
+  /*/OBLIGA A LLENAR TODOS LOS CAMPOS
+  if (numeroInput.value !== " " && nombreInput.value !== " " && mesInput.value !== " " && añoInput.value !== " ") {
+    alert("Debes llenar todos los campos");
+  } else {
+    console.log("ok")
+  }*/
+  
+  //LLAMAR DE VALIDATOR PARA ENMASCARAR 
+  const enmascarar = validator.maskify(tarjetaCliente);
+  console.log(enmascarar);
+
+  //LLAMAR DE VALIDATOR PARA QUE DIGA SI ES VALIDA O NO   
+  const resultado = validator.isValid(tarjetaCliente);
+  if (resultado === true) {
+    //ABRIR POP UP
+    abrirPopUp();
+    //alert("Tu tarjeta es valida");
+  } else if (resultado === false) {
+    abrirPopUpError();
+    //alert("Tu tarjeta es invalida");
+  }
+});
+
+//POP UP VALIDO
+const cerrarPopUp = document.querySelector('#btn-cerrar-popUp');
+const popUp = document.querySelector('#popUp');
+
+function abrirPopUp(){
+  popUp.showModal();
+}
+
+cerrarPopUp.addEventListener("click", () => {
+  popUp.close();
+})
+
+//POP UP INVALIDO
+const cerrarPopUpError = document.querySelector('#btn-cerrar-popUpError');
+const popUpError = document.querySelector('#popUpError');
+
+function abrirPopUpError(){
+  popUpError.showModal();
+}
+
+cerrarPopUpError.addEventListener("click", () => {
+  popUpError.close();
+})
+
+
+
+//EN ADELANTE TODO ES ÚNICAMENTE PARA CLONAR LA INFO EN LA TARJETA DIGITAL 
+
+
+//TRAER NOMBRE DE LA TARJETA
+const nombreCard = document.querySelector(".datos-tarjeta-nombre");
+const nombreInput = document.querySelector("#nombre-cliente");
+
+//DUPLICAR NOMBRE EN TARJETA DIGITAL
+nombreInput.addEventListener("input", () => {
+  if (nombreInput.value === '') {
+    nombreCard.innerText = "Nombre de la tarjeta"
+  } else {
+    nombreCard.innerText = nombreInput.value;
+  }
+});
+
+//TRAER FECHA DE LA TARJETA
+const mesCard = document.querySelector(".datos-tarjeta-mes");
+const añoCard = document.querySelector(".datos-tarjeta-año");
+const mesInput = document.querySelector("#mes-cliente");
+const añoInput = document.querySelector("#año-cliente");
+
+
+//DUPLICAR MES EN TARJETA DIGITAL
+mesInput.addEventListener('input', event => {
+  const mesValue = event.target.value;
+  mesInput.value = mesValue.replace(/\s/g, '').replace(/\D/g, '') 
+  if (mesInput.value === '') {
+    mesCard.innerText = "MM"
+  } else {
+    mesCard.innerText = mesInput.value
+  }
+});
+
+//DUPLICAR AÑO EN TARJETA DIGITAL
+añoInput.addEventListener('input', event => {
+  const añoValue = event.target.value;
+  añoInput.value = añoValue.replace(/\s/g, '').replace(/\D/g, '')
+
+  if (añoInput.value === '') {
+    añoCard.innerText = "YY"
+  } else {
+    añoCard.innerText = añoInput.value
+  }
+});
+
+/*/CONOCER EL BANCO DE LA TARJETA 
+const bancos = [
+  {nombre: "VISA" , numero: "1234"},
+  {nombre: "MASTERCARD" , numero: "5678"},
+  {nombre: "AMERICANEXPRESS" , numero: "9012"}
+];
+
+
+
+//SACAR LOGO BANCO
+const logoBanco = document.getElementById('logo-banco');
+logoBanco.innerHTML = '';
+    const imagen = document.createElement('img');
+    imagen.src = "images\VISA.png";
+    logoBanco.appendChild(imagen);*/
+
---
+++ validator.js
@@ -0,0 +1,47 @@
+const validator = {
+  isValid:function(cadena){
+    const cardCliente = cadena.split(""); //convertir en array
+    const reversaCard = cardCliente.reverse(); //invertir 
+    console.log(reversaCard);
+
+    for (let i=0; i<reversaCard.length; i++){
+      if (i%2 !== 0){ // busca los indices de posicion par
+        reversaCard[i] = reversaCard[i] * 2; // multiplica los pares x2
+      }
+      if (reversaCard[i] > 9){ //ve si el índice es mayor a 9
+        reversaCard[i] = parseInt(String(reversaCard[i]).charAt(0)) + parseInt(String(reversaCard[i]).charAt(1)) //suma los 2 digitos
+        console.log(reversaCard);
+      }
+    } 
+
+    let sumar = 0; //declaramos la suma para empezar en 0
+      
+    for (let i=0; i<reversaCard.length; i++) {
+      sumar += parseInt(reversaCard[i]); //suma todos los digitos
+      console.log(sumar);
+    }
+    
+    const sumar2 = sumar * 9 % 10; //se mutiplica y divide 
+    console.log(sumar2);
+
+    if (sumar2 === 0){ //condicional para ver si es valida
+      //alert("Tu tarjeta es valida")
+      return true
+    }else{
+      //alert("Tu tarjeta es invalida")
+      return false
+    }      
+  },
+
+  //FUNCIÓN MASKIFY
+  maskify:function(cardCliente){
+    for (let i = 0; i <cardCliente.length; i++) {   
+      return cardCliente.slice(0, -4).replace(/./g, "#") + cardCliente.slice(-4);
+    }   
+  }  
+};
+
+
+
+export default validator;
+
